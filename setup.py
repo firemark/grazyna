a535db0951831ed7ae728430b410dba80d693a9b
Firemark <marpiechula@gmail.com>
2015-07-10 00:14:19 +0200

    add new version

--- setup.py
+++ setup.py
@@ -10,8 +10,8 @@ requires = [
 
 setup(
     name='grazyna',
-    version='0.5.1',
-    download_url='https://github.com/firemark/grazyna/tarball/0.5.1',
+    version='0.5.2',
+    download_url='https://github.com/firemark/grazyna/tarball/0.5.2',
     description='Grazyna The irc bot',
     long_description='Grazyna The irc bot',
     classifiers=[],
