fb3676e534db5948a77a1d7f5af8109416f03f4d
swoldanski <sebastian.woldanski@gmail.com>
2017-05-21 20:49:34 +0200

    a few fixes to please pylint

--- grazyna/plugins/weekend.py
+++ grazyna/plugins/weekend.py
@@ -1,18 +1,18 @@
+"""Answer to timeless question - are we at .weekend, yet?"""
 from datetime import datetime
 
 from grazyna.utils import register
 
 @register(cmd='weekend')
 def weekend(bot):
-    """
-    Answer to timeless question - are we at .weekend, yet?
-    """
+    """Answer to timeless question - are we at .weekend, yet?"""
     current_date = datetime.now()
     day = current_date.weekday()
     answer = "WTF?"
     if day in (5, 6):
         answer = "Oczywiście %s - jest weekend. Omawiamy tylko lajtowe tematy, ok?" % bot.nick
     else:
-        answer = "%s - dopiero %s, musisz jeszcze poczekać..." % (bot.nick, datetime.strftime(current_date, "%A"))
+        answer = "%s - dopiero %s, musisz jeszcze poczekać..." \
+        % (bot.nick, datetime.strftime(current_date, "%A"))
 
     bot.reply(answer)
