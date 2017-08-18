2686867841162c566c530b67b2f3a78931d309d3
Firemark <marpiechula@gmail.com>
2017-08-18 13:58:20 +0200

    test db scope and whois on irc sender

--- grazyna/tests/test_client.py
+++ grazyna/tests/test_client.py
@@ -1,8 +1,12 @@
 from asyncio import Future
 from grazyna.irc.client import IrcClient as DefaultIrcClient
+from grazyna.irc.models import WhoisData
 from grazyna.test_mocks.sender import IrcSender, Message
 from grazyna.config import create_empty_config
 from unittest.mock import Mock, patch, call
+
+import pytest
+import asyncio
 
 
 class IrcClient(DefaultIrcClient, IrcSender):
@@ -66,3 +70,29 @@ def test_connection_lost():
     client.connection_lost(exc)
     assert client.connection_lost_future.exception() == exc
     assert client.importer.is_canceled is True
+
+
+def test_db_scope():
+    client = make_client(main=dict(db_uri='sqlite://'))
+    session = client.get_session()
+    assert session is not None
+
+
+@pytest.mark.asyncio
+def test_whois():
+    client = make_client()
+
+    @asyncio.coroutine
+    def set_whois():
+        yield from asyncio.sleep(0.5)
+        future = client.whois_heap['socek']
+        future.data.nick = 'socek'
+        future.set_result(future.data)
+
+    result, _ = yield from asyncio.gather(
+        client.whois('socek'),
+        set_whois()
+    )
+
+    assert isinstance(result, WhoisData)
+    assert result.nick == 'socek'
