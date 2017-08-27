abb330ea29dee5b73497ebf860f3c84dffc91d19
Firemark <marpiechula@gmail.com>
2017-08-27 14:04:33 +0200

    fix tests

--- grazyna/tests/test_types.py
+++ grazyna/tests/test_types.py
@@ -5,11 +5,8 @@ import pytest
 
 def test_range_int():
     typer = range_int(1, 3)
-    with pytest.raises(TypeError):
-        typer('4')
-    with pytest.raises(TypeError):
-        typer('0')
-
+    assert typer('4') == 3
+    assert typer('0') == 1
     assert typer('2') == 2
 
 
@@ -19,9 +16,15 @@ def test_range_int_without_ranges():
 
 
 def test_is_chan():
-    assert is_chan('#czarnobyl') is True
-    assert is_chan('socek') is False
+    assert is_chan('#czarnobyl') == '#czarnobyl'
+    with pytest.raises(TypeError):
+        is_chan('socek')
 
 
 def test_regexp():
-    typer = is_chan()
+    typer = regexp(r'001+')
+    match = typer('0011')
+    assert match.group(0) == '0011'
+    with pytest.raises(TypeError):
+        typer('2')
+
